75129ee602d079601cd31788dfeb1de338612155
Ohad Ripsthos <105322785+Ripshtos@users.noreply.github.com>
2023-05-29 16:09:29 +0300

    finished game

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 var counter = 0;
 var lastClicked;
 var lastClickedid;
-
+var strMisson;
 var minutes = 0;
 var seconds = 0;
 var intervalId;
 
-window.onload = function() {
-   timerElement = document.getElementById('timer');
-   seconds = 0;
-   intervalId;  
+var countWin = 0;
+
+window.onload = function () {
+  timerElement = document.getElementById("timer");
+  seconds = 0;
+  intervalId;
 };
 
 const sleep = async (milliseconds) => {
-  await new Promise(resolve => {
-      return setTimeout(resolve, milliseconds)
+  await new Promise((resolve) => {
+    return setTimeout(resolve, milliseconds);
   });
 };
 
 // Usage:
 function freezeWebsite() {
   // Disable user interactions during the freeze
-  document.body.style.pointerEvents = 'none';
+  document.body.style.pointerEvents = "none";
 
-  setTimeout(function() {
+  setTimeout(function () {
     // Enable user interactions after the freeze
-    document.body.style.pointerEvents = 'auto';
+    document.body.style.pointerEvents = "auto";
   }, 3000);
 }
-
 
 function updateTimer() {
   seconds++;
@@ -36,7 +37,7 @@ function updateTimer() {
     seconds = 0;
     minutes++;
   }
-  timerElement.textContent = minutes + ' minutes ' + seconds + ' seconds';
+  timerElement.textContent = minutes + " minutes " + seconds + " seconds";
 }
 
 function startTimer() {
@@ -47,26 +48,21 @@ function stopTimer() {
   clearInterval(intervalId);
 }
 
-
 function hidePics() {
   var parent = document.getElementById("row");
-  var divs = parent.getElementsByTagName('div');
+  var divs = parent.getElementsByTagName("div");
   for (var i = 0; i < divs.length; i++) {
     var div = divs[i];
-    var img = div.getElementsByTagName('img')[0];
+    var img = div.getElementsByTagName("img")[0];
     div.style.backgroundImage = 'url("icon.jpg")';
-    div.style.backgroundSize = 'contain';
-    img.style.visibility = 'hidden';
+    div.style.backgroundSize = "contain";
+    img.style.visibility = "hidden";
   }
-
 }
-
-
-
 
 function shuffle() {
   var parent = document.getElementById("row");
-  var divs = Array.from(parent.getElementsByTagName('div'));
+  var divs = Array.from(parent.getElementsByTagName("div"));
 
   // Shuffle the array using Fisher-Yates algorithm
   for (var i = divs.length - 1; i > 0; i--) {
@@ -87,27 +83,25 @@ function shuffle() {
   }
 }
 
-
 function separateString(inputString) {
-  var numbersString = inputString.replace(/\D/g, '');
-  var charsString = inputString.replace(/[0-9]/g, '');
-  
+  var numbersString = inputString.replace(/\D/g, "");
+  var charsString = inputString.replace(/[0-9]/g, "");
+
   return [numbersString, charsString];
 }
 
 async function handleButtonClick() {
-
   var inputs = document.getElementById("inputs");
 
   var table = document.getElementById("myTable");
-  
+
   var timer = document.getElementById("timerC");
 
   var name = document.getElementById("name").value.trim();
 
-  document.getElementById("nameC").textContent ="Player name : " + name;
+  document.getElementById("nameC").textContent = "Player name : " + name;
 
-  const strMisson = document.getElementById("mission").value.trim();
+  strMisson = document.getElementById("mission").value.trim();
 
   console.log(strMisson);
   if (strMisson > 60) {
@@ -115,7 +109,7 @@ async function handleButtonClick() {
     return;
   }
 
-  if ( strMisson % 2 != 0) {
+  if (strMisson % 2 != 0) {
     alert("must be an even number of cards please");
     return;
   }
@@ -124,101 +118,94 @@ async function handleButtonClick() {
     alert("must be more than 2 cards");
     return;
   }
- 
-// Get all the div elements within the table
-var divs = table.querySelectorAll('div');
 
-// Loop to delete the specified number of divs while excluding the specific row
-for (var i = 0; i < 60 - strMisson; i++) {
-  // Get the current div element
-  var div = divs[i];
+  // Get all the div elements within the table
+  var divs = table.querySelectorAll("div");
 
-  // Check if the div belongs to the excluded row
-  if (div.id == "row") {
-    continue; // Skip to the next iteration if the div is in the excluded row
+  // Loop to delete the specified number of divs while excluding the specific row
+  for (var i = 0; i < 61 - strMisson; i++) {
+    // Get the current div element
+    var div = divs[i];
+
+    // Check if the div belongs to the excluded row
+    if (div.id == "row") {
+      continue; // Skip to the next iteration if the div is in the excluded row
+    }
+
+    // Remove the div element from the table
+    div.remove();
   }
 
-  // Remove the div element from the table
-  div.remove();
-}
-
-
   shuffle();
-  inputs.style.visibility= "hidden";
+  inputs.style.visibility = "hidden";
   table.style.visibility = "visible";
-  timer.style.visibility ='visible';
+  timer.style.visibility = "visible";
   await sleep(10000);
   hidePics();
   startTimer(); // Start the timer
-
- 
 }
 
-
-
-
-function Correct(image_clicked)
-{
+function Correct(image_clicked) {
   var div1 = document.getElementById(image_clicked);
   if (div1) {
-    div1.style.visibility = 'hidden';
+    div1.style.visibility = "hidden";
   }
 
   var div2 = document.getElementById(lastClicked + lastClickedid);
   if (div2) {
-    div2.style.visibility = 'hidden';
+    div2.style.visibility = "hidden";
   }
-  
+
   image_clicked = "";
   lastClicked = "";
 
-  var win = document.getElementById("row");
-  if (win) {
-    var childDivs = win.querySelectorAll('div');
-    if(childDivs.length == 0)
+  countWin = countWin + 2;
+  if (countWin == strMisson)
+  {
+    sleep(2000);
+    if (alert("Congrats ! you have finished the game in : " + minutes + " minutes and " + seconds + " seconds,\n Click ok to play again" )) 
     {
-      alert("win"); 
     }
+     else window.location.reload();
   }
-
 }
 
-async function HandleImageClick(element)
-{
+async function HandleImageClick(element) {
   console.log(element.id);
 
   var string = separateString(element.id);
   var image_id = string[0];
   var image_clicked = string[1];
-  
+
   var parent = document.getElementById(element.id);
-  var img = parent.getElementsByTagName('img');
+  var img = parent.getElementsByTagName("img");
 
   parent.style.backgroundImage = "none";
 
   for (var i = 0; i < img.length; i++) {
-    img[i].style.visibility = 'visible';
+    img[i].style.visibility = "visible";
   }
 
-  if(counter == 0)
-  {
+  if (counter == 0) {
     lastClicked = image_clicked;
     lastClickedid = image_id;
     counter++;
     return;
   }
-  
+
   freezeWebsite();
 
-  if(counter == 1 && lastClicked == image_clicked && image_id != lastClickedid )
-  {
+  if (
+    counter == 1 &&
+    lastClicked == image_clicked &&
+    image_id != lastClickedid
+  ) {
     counter = 0;
     await sleep(3000);
     Correct(element.id);
   }
 
-  if(counter == 1 || image_id != lastClickedid )
-  {
+  if (counter == 1 || image_id != lastClickedid) {
     counter = 0;
     lastClicked = "";
     lastClickedid = "";
@@ -226,5 +213,4 @@ async function HandleImageClick(element)
     hidePics();
     return;
   }
-
 }
